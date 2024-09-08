ee935e6e72b6180e8c84de43b3f01d05bb6bf23c
CrisPPereira <chaleiracriativa@gmail.com>
2024-09-08 02:56:26 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {
+        titulo : "Samantha Maloney",
+        descricao : "é uma baterista norte-americana renomada por sua habilidade e versatilidade, tendo tocado em bandas de diversos gêneros musicais. Conhecida por sua passagem por bandas como Hole e Mötley Crüe, Samantha se destaca no cenário musical por sua técnica apurada e presença de palco marcante" ,
+        link : "https://en.wikipedia.org/wiki/Samantha_Maloney",
+        tags: "Mötley Crüe"
+    
+    },
+
+    {
+    titulo : "Meg White",
+    descricao : "é uma baterista americana, mais conhecida por sua participação na banda The White Stripes. Seu estilo único e minimalista, juntamente com a batida constante e impulsionadora, contribuiu significativamente para o sucesso da banda." ,
+    link : "https://en.wikipedia.org/wiki/Meg_White",
+    tags: "White Stripes minimalista"
+
+},
+
+{
+    titulo : "Maureen Tucker",
+    descricao : "é uma baterista americana, conhecida por sua participação na banda The Velvet Underground. Seu estilo de tocar, caracterizado por batidas simples e repetitivas, mas com um groove único, influenciou diversas gerações de músicos." ,
+    link : "https://en.wikipedia.org/wiki/Maureen_Tucker",
+    tags: "The Velvet Underground"
+
+}
+]
+
